0b52951845895188334440e1d0988240a258aaf1
Tokuhiro Matsuno <tokuhirom@linecorp.com>
2023-12-11 13:28:53 +0900

    Move configuration to frontend

--- src/lib.rs
+++ src/lib.rs
@@ -1,54 +1,31 @@
 extern crate serde_json;
 
 use lindera_analyzer::analyzer::Analyzer;
-use serde_json::json;
+use serde_json::Value;
 use wasm_bindgen::prelude::*;
 
-
 #[wasm_bindgen]
-pub fn analyze(text: &str) -> Result<JsValue, JsValue> {
-    let analyzer = Analyzer::from_value(&json!({
-  "character_filters": [
-    {
-      "kind": "unicode_normalize",
-      "args": {
-        "kind": "nfkc"
-      }
-    },
-    {
-      "kind": "japanese_iteration_mark",
-      "args": {
-        "normalize_kanji": true,
-        "normalize_kana": true
-      }
+pub fn analyze(text: &str, configuration: &str) -> Result<JsValue, JsValue> {
+  let args = match serde_json::from_slice::<Value>(configuration.as_ref()) {
+    Ok(args) => { args }
+    Err(e) => {
+      return Err(serde_wasm_bindgen::to_value(
+        &format!("JSON deserialization error: {:?}", e))?)
     }
-  ],
-  "tokenizer": {
-    "dictionary": {
-      "kind": "ipadic"
-    },
-    "mode": "normal"
-  },
-  "token_filters": [
-    {
-      "kind": "japanese_katakana_stem",
-      "args": {
-        "min": 3
-      }
+  };
+
+  let analyzer = Analyzer::from_value(&args).unwrap();
+
+  println!("text: {}", text);
+
+  // tokenize the text
+  let tokens = match analyzer.analyze(&text) {
+    Ok(o) => { o}
+    Err(e) => {
+      return Err(serde_wasm_bindgen::to_value(&format!("Lindera error: {:?}", e))?)
     }
-  ]
-        })).unwrap();
+  };
 
-    println!("text: {}", text);
-
-    // tokenize the text
-    let tokens = match analyzer.analyze(&text) {
-      Ok(o) => { o}
-      Err(e) => {
-        return Err(serde_wasm_bindgen::to_value(&format!("Lindera error: {:?}", e))?)
-      }
-    };
-
-    // JSに対応した戻り値を返すので
-    Ok(serde_wasm_bindgen::to_value(&tokens)?)
+  // JSに対応した戻り値を返すので
+  Ok(serde_wasm_bindgen::to_value(&tokens)?)
 }
